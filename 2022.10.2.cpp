75ba561e1f074a6fc88129bcf2cad318a7dda931
xiexing123 <2182954989@qq.com>
2022-10-03 00:09:16 +0800

    2022.10.2

---
+++ 2022.10.2.cpp
@@ -0,0 +1,156 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	if (a < 18)
+//	{
+//		printf("未成年\n");
+//		printf("不能谈恋爱\n");
+//	}
+//	else
+//	{
+// if (a >= 18 && a < 30)
+//	printf("青年\n");
+//else if (a > 30 && a <= 50)
+//printf("壮年\n");
+//else if (a >= 50 && a <= 90)
+//printf("老年\n");
+//else
+//printf("老不死\n");
+//
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0, b = 2;
+//	if (a == 1)
+//	{
+//		if (b == 2)
+//			printf("hehe\n");
+//	}
+//	else
+//		printf("haha\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("如果a=%d", &a);
+//	if (a % 2 == 1)
+//	{
+//		printf("则a是奇数\n");
+//	}
+//	else
+//	{
+//		printf("则a是偶数\n");
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int day= 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	case 3:
+//		printf("星期四\n");
+//		break;
+//	default:
+//		printf("输入错误\n");
+//	}
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	while (a <= 10)
+//	{
+//		a++;
+//		if (a == 5)
+//	    continue;
+//		printf("%d\n", a);
+//		
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	
+//	while ((a = getchar()) !='d')
+//	{
+//		putchar(a);
+//	}
+//	putchar(a); 
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	for (a = 0; a <= 10; a++)
+//	{
+//		for (b = 0; b <= 10; b++)
+//		{
+//			printf("06 ");
+//		}
+//
+//	}
+//	return 0;
+//}
+#/*include<stdio.h>
+int main()
+{
+	int a = 1;
+	do
+	{
+		printf("%d\n", a);
+		a++;
+	}
+		while (a <= 9);
+	return 0;
+	}*/
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int n = 10;
+//	int c = 1;
+//	int b = 1;
+//	int d = 1;
+//	int e = 1;
+//	int sum = 0;
+//	for (a = 1; a <=4; a++)
+//	{
+//		if(a!=4)
+//		c =c * a ;
+//		continue;
+//		if (a != 3)
+//			b = b * a;
+//		continue;
+//		if (a != 2)
+//			d = d * a;
+//		continue;
+//		if (a != 1)
+//			e = e * a;
+//		sum = a + b + c + d + e;
+//	}		
+//	printf("n的阶乘=%d ", sum);
+//	
+//	return 0;
+//}
